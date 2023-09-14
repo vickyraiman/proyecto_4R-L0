443f6d9f0857d224745e7ab9ced930659b45a556
dantelern <110490913+dantelern@users.noreply.github.com>
2023-09-14 14:32:28 -0300

    arreglé el pwm

--- servo_code.py
+++ servo_code.py
@@ -1,4 +1,7 @@
 import machine
+import sys
 
-p = machine.Pin(11)
-pwm = machine.PWM(7)
+p = machine.Pin(4)
+pwm = machine.PWM(machine.Pin(15))
+
+# sys.path.append('C:\Python310\Lib\site-packages')
