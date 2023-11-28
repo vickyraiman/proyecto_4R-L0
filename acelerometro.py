5683b8ca0ac589f6d8d2beccec06a445a8ab8a3d
dantelern <110490913+dantelern@users.noreply.github.com>
2023-11-28 02:09:46 -0300

    NUEVO UPDATE ACELEROMETRO

--- acelerometro.py
+++ acelerometro.py
@@ -2,13 +2,13 @@ import time
 from mpu6050 import mpu6050
 import RPi.GPIO as GPIO
 
-# pines del modelo de figma
+# Pines del modelo de figma
 SDA_PIN = 2
 SCL_PIN = 3
-MOTOR_PIN_1A = 17  
-MOTOR_PIN_1B = 27  
-MOTOR_PIN_2A = 19  
-MOTOR_PIN_2B = 26 
+MOTOR_PIN_1A = 17
+MOTOR_PIN_1B = 27
+MOTOR_PIN_2A = 19
+MOTOR_PIN_2B = 26
 
 sensor = mpu6050(address=0x68, bus=1, scl=SCL_PIN, sda=SDA_PIN)
 
@@ -22,14 +22,14 @@ def controlar_motores():
     lectura = sensor.get_accel_data()
     eje_y = lectura['y']
 
-  if eje_y > 0:
+    if eje_y > 0:
         GPIO.output(MOTOR_PIN_1A, GPIO.HIGH)
         GPIO.output(MOTOR_PIN_1B, GPIO.LOW)
     else:
         GPIO.output(MOTOR_PIN_1A, GPIO.LOW)
         GPIO.output(MOTOR_PIN_1B, GPIO.HIGH)
 
-    if eje_y > 0:
+    if eje_y < 0:
         GPIO.output(MOTOR_PIN_2A, GPIO.HIGH)
         GPIO.output(MOTOR_PIN_2B, GPIO.LOW)
     else:
